92edc50b3262d4c465e5946c8f0c7a5e97602e83
Kaylee <kaylee.mchugh@credera.com>
2019-07-28 20:11:54 -0600

    added in rest client

---
+++ src/main/java/com/armyPOC/emailService/config/RestClient.java
@@ -0,0 +1,45 @@
+package com.armyPOC.emailService.config;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.Email;
+import java.net.URI;
+
+public class RestClient
+{
+
+   public static final String SERVER = "https://prod-80.westus.logic.azure.com";
+   private HttpStatus status;
+   private RestTemplate restTemplate;
+   private HttpHeaders httpHeaders;
+
+   RestClient() {
+      restTemplate = new RestTemplate();
+      httpHeaders = new HttpHeaders();
+      httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+   }
+
+   public String get(URI uri) {
+      HttpEntity<String> requestEntity = new HttpEntity<String>("", httpHeaders);
+
+      ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, requestEntity, String.class);
+      this.setStatus(responseEntity.getStatusCode());
+      return responseEntity.getBody();
+   }
+
+   void post(URI uri, Email email) {
+      ResponseEntity<Email> responseEntity = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(email, httpHeaders),  new ParameterizedTypeReference<Email>(){});
+      this.setStatus(responseEntity.getStatusCode());
+   }
+
+   public HttpStatus getStatus() {
+      return status;
+   }
+
+   private void setStatus(HttpStatus status) {
+      this.status = status;
+   }
+}
